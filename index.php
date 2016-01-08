47ca80f555c48cab13def93ac5402bb078ef5d00
YAP CHENG WEI <yapchengwei@gmail.com>
2016-01-09 00:06:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,129 @@
+<DOCTYPE html>
+<head>
+	<title>Sales of Point System for Video and Audio shop 
+	</title>
+	<link rel="icon" href="images/favicon.png" type="image/x-icon" sizes="16x16">
+	<link href="css/sopsys.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div class="header section group">
+		header
+	</div>
+	<div class="nav_bar section group">
+		<div class="col span_1_of_2">
+		nav bar
+		</div>
+	</div>
+	<div class="cont section group">
+		<div class="cont_element col span_1_of_2">
+		content column 1
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		blabla<br>
+		
+
+		</div>
+		<div class="cont_element col span_1_of_2">
+		content column 2
+		</div>
+		
+		<div class="footer">
+		</div>
+	</div>
+</body>
+</html>
