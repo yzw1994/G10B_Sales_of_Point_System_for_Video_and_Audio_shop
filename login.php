914a6d929872fe1538af19bc0299e003a8b98f5a
YAP CHENG WEI <yapchengwei@gmail.com>
2016-01-15 00:14:35 +0800

    Login page interface
    login page css, page done.

--- login.php
+++ login.php
@@ -1,11 +1,31 @@
 <DOCTYPE html>
 <html>
 <head>
-	<link href="css/main.css" rel="stylesheet" type="text/css" />
+	<title>Log In</title>
+	<link href="css/login_page.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/font/elegantlux/elegant_luxmager.css" type="text/css" charset="utf-8" />
 </head>
 <body>
-	<>
-	<>
+	<div id="" class="login_div">
+		<div id="" class="logo_div">
+			<img src="images/logo.ico" alt="Blu Video and Audio Shop" id="" class="logo_images"/>
+		</div>
+		<div id="" class="input_div">
+			<form name="login" method="" action="">
+				<span>whatever slogan here...</span>
+				<input type="text" name="email" value="" id="" placeholder="Your Email..." class="input_box email"/>
+				<input type="password" name="password" value=""  id="" placeholder="Your Password..." class="input_box password"/>
+				<div id="" class="login_error">
+					<span id="" class="error_message">Invalid Email or Password.
+					</span>
+				</div>
+				<input type="submit" value="REGISTER" id="" class="btn register_btn"/>
+				<input type="submit" value="LOGIN" id="" class="btn login_btn"/>
+			</form>
+			<div class=""></div>
+		</div>
+		
+	</div>
 	
 </body>
 </html>
