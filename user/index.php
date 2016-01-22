4c54b6752e8e2c7d337c2464f90e130baa91e539
YAP CHENG WEI <yapchengwei@gmail.com>
2016-01-22 19:27:29 +0800

    new folder to store universal part

--- user/index.php
+++ user/index.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Sales of Point System for Video and Audio shop 
+	</title>
+	<link rel="icon" href="images/favicon.png" type="image/x-icon" sizes="16x16">
+	<link href="../css/main.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/font/elegantlux/elegant_luxmager.css" type="text/css" charset="utf-8" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="../js/main.js"></script>
 </head>
 <body>
-    <p>cw love lilian lai pei </p>
+    <div class="cont section group">
+	
+		<!--<?php include("header.php");?>-->
+		<?php include("navigation.php");?>
+		<div class="cont_element col span_2_of_2 slider_div">
+		<?php include("slider.php");?>
+		</div>
+		<div class="cont_element col span_1_of_2 pro_char">
+			<?php include("leftprod.php");?>
+		</div>
+		<div class="cont_element col span_1_of_2 pro_char">
+			<?php include("rightprod.php");?>
+		</div>
+		<?php include("../utility/footer.php");?>
+		
+	</div>
 </body>
-<<<<<<< HEAD
-
-
 </html>
-=======
-</html>
->>>>>>> origin/master
