8497362ad0354b3c1cd6f06b4fcdd6d56897a9f0
YAP CHENG WEI <yapchengwei@gmail.com>
2016-01-23 13:49:03 +0800

    user profile

--- user/user_profile.php
+++ user/user_profile.php
@@ -1,10 +1,83 @@
-<!DOCTYPE html>
+<DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<head>
+	<title>Sales of Point System for Video and Audio shop
+	</title>
+	<link rel="icon" href="images/favicon.png" type="image/x-icon" sizes="16x16">
+	<link href="../css/main.css" rel="stylesheet" type="text/css" />
+  <link href="../css/user_profile.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/font/elegantlux/elegant_luxmager.css" type="text/css" charset="utf-8" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="../js/main.js"></script>
 
-  </body>
+</head>
+<body>
+	<div class="cont section group">
+
+		<?php include("../utility/user_header.php");?>
+		<?php include("../utility/navigation.php");?>
+		<div class="cont_element col span_2_of_2">
+        <div id="" class="span_1_of_4 user_pic_div">
+          <div id="" class="user_pic_box">
+            <img src="../images/profile_pic.jpg" id="" class="user_pic_img" name="" title=""/>
+
+          </div>
+        </div>
+        <div id="" class="span_3_of_4 user_detail_div">
+          <div id="" class="user_data">
+            <span id="" class="user_data_title">
+              Email
+            </span>
+            <span id="" class="user_data_content">
+              Yap Cheng Wei
+            </span>
+            <div id="" class="user_data_edit">
+            </div>
+          </div>
+          <div id="" class="user_data">
+            <span id="" class="user_data_title">
+              Name
+            </span>
+            <span id="" class="user_data_content">
+              Yap Cheng Wei
+            </span>
+            <div id="" class="user_data_edit">
+            </div>
+          </div>
+          <div id="" class="user_data">
+            <span id="" class="user_data_title">
+              Contact No
+            </span>
+            <span id="" class="user_data_content">
+              Yap Cheng Wei
+            </span>
+            <div id="" class="user_data_edit">
+            </div>
+          </div>
+          <div id="" class="user_data">
+            <span id="" class="user_data_title">
+              Address
+            </span>
+            <span id="" class="user_data_content">
+              Yap Cheng Wei
+            </span>
+            <div id="" class="user_data_edit">
+            </div>
+          </div>
+          <div id="" class="user_data">
+            <span id="" class="user_data_title">
+              Birthday
+            </span>
+            <span id="" class="user_data_content">
+              Yap Cheng Wei
+            </span>
+            <div id="" class="user_data_edit">
+            </div>
+          </div>
+        </div>
+      </div>
+		<?php include("../utility/footer.php");?>
+
+	</div>
+</body>
 </html>
