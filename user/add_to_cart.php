b74ee71265e89a54aa0c4a26c4abd91d188ca3a4
YAP CHENG WEI <yapchengwei@gmail.com>
2016-01-24 01:39:02 +0800

    cart page

--- user/add_to_cart.php
+++ user/add_to_cart.php
@@ -17,23 +17,61 @@
 		<?php include("../utility/user_header.php");?>
 		<?php include("../utility/navigation.php");?>
 		<div class="cont_element col span_2_of_2">
-      <a href="" id="" class="cart_prod_div span_5_of_5">
-          <div id="" class="span_1_of_5 right_border">
-            <div id="" class="cart_prod_image">
-              <img src="../images/cover1.jpg" id="" class=""/>
+      <div id="" class="cart_prod_div span_5_of_5">
+          <a href="" target="_blank" id="" class="cart_prod_link">
+            <div id="" class="span_1_of_5 right_border">
+              <div id="" class="cart_prod_image">
+                <img src="../images/cover1.jpg" id="" class=""/>
+              </div>
             </div>
-          </div>
+          </a>
           <div id="" class="span_4_of_5">
-            <span id="" class="cart_prod_title">
-              adelle
-            </span>
+              <span id="" class="cart_prod_title">
+                <a href="" target="_blank" id="" class="cart_prod_link">adelle</a>
+              </span>
+
             <span id="" class="cart_prod_checkbox">
             </span>
-            <span id="" class="cart_prod_description">
+            <span id="" class="cart_prod_description right_border_dotted">
               jshb asjnxja,lsxnajsm xsjanxja asjnxja xbjsvaddssjhsc aksnc kasnnkjx sabxaskcjcnkbckb ashcb bnjxh bmjsa b
             </span>
+            <span id="" class="cart_prod_price right_border_dotted">
+              RM 9.90
+            </span>
+            <span id="" class="cart_prod_price right_border_dotted">
+              Amount
+            </span>
           </div>
-      </a>
+      </div>
+      <div id="" class="cart_control_div">
+
+        <span id="" class="cart_select_all right_border_white">
+          <input type="checkbox" name="deleteall" id="" class="cart_delete_all"/>
+          SELECT ALL
+        </span>
+        <span id="" class="cart_delete_all right_border_white">
+          DELETE ALL
+        </span>
+
+
+        <span id="" class="cart_item_amount right_border_white">
+          CHOOSED ITEM :
+          <span id="" class="cart_count_result">
+            1
+          </span>
+        </span>
+
+        <span id="" class="cart_count_total">
+          TOTAL :
+          <span id="" class="cart_count_result">
+            RM 9.90
+          </span>
+        </span>
+
+        <button id="" class="cart_purchase_btn">
+          PURCHASE
+        </button>
+      </div>
 		</div>
 
 		<?php include("../utility/footer.php");?>
