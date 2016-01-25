e8c99b5336f17b50bb978a6944d5dc2c3c5fa798
Koh Jin Yi 1121115790 <jinyi_313@hotmail.com>
2016-01-26 00:01:57 +0800

    delete file

--- test/admin_delete_product.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include("dataconn.php");
-	$product_id=(int)$_GET['pid'];	
-	mysql_query("delete from product where product_id=$product_id");
-	header("Location:admin_view_product.php");
-	
-?>
--- test/dataconn.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host="localhost";
-	$user="root";
-	$password="";
-	mysql_connect($host,$user,$password);
-	mysql_select_db("SHOP");
-	
-	session_start();
-?>
