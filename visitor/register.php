768db7f58afb52ba99192bf202a7cf1a22a25522
KOH CHEE GUAN 1121116127 <yuanyuan0331@live.com>
2016-01-25 23:28:37 +0800

    visitor register  with php code

--- visitor/register.php
+++ visitor/register.php
@@ -1,4 +1,55 @@
 <html>
+
+<?php
+	 include("../dataconn/dataconn.php");
+
+
+		if(isset($_POST["createaccbtn"]))
+		{
+
+			$email=$_POST["email"];
+			$pass=$_POST["password"];
+			$repass=$_POST["repassword"];
+			$phone=$_POST["phone"];
+			$bday=$_POST["bday"];
+			$address=$_POST["address"];
+			$name=$_POST["text"];
+
+			$emailresult=mysql_query("select * from user where User_Email='$email'");
+
+			if(mysql_num_rows($emailresult)!=0)
+		{
+			?>
+			<script type="text/javascript">
+				alert("Your email is exist. Please change other email.");
+			</script>
+			<?php
+		}
+		else
+		{
+			mysql_query("insert into user(User_Name,User_Password,User_Email,User_Phone,User_Address,User_Dob,User_Subscribe_Status,User_Privilege)values('$name','$pass','$email','$phone','$address','$bday','1','2')");
+			?>
+			<script type="text/javascript">
+				alert("Your register is successful.");
+				window.location.href='visitor.php';
+			</script>
+
+			<?php
+
+
+		}
+
+
+
+
+		}
+
+
+
+
+?>
+
+
 <head>
 	<title>Log In</title>
 	<link href="../css/login_page.css" rel="stylesheet" type="text/css" />
@@ -10,7 +61,7 @@
 			<a href="visitor.php"><img src="../images/logo.ico" alt="Blu Video and Audio Shop" id="" class="logo_images"/></a>
 		</div>
 		<div id="" class="input_div">
-			<form name="login" method="" action="">
+			<form name="login" method="post" action="">
 				<span>whatever slogan here...</span>
 				<input type="text" name="email" value="" id="" placeholder="Your Email..." class="input_box email"/>
 				<span id="" class="reg_error">
@@ -33,7 +84,7 @@
 				<input type="text" name="address" value=""  id="" placeholder="Your Address..." class="input_box address"/>
 				<span id="" class="reg_error">
 				</span>
-				<input type="submit" value="REGISTER" id="" class="btn press_reg "/>
+				<input type="submit" value="REGISTER" id="" class="btn press_reg" name="createaccbtn"/>
 			</form>
 			<div class=""></div>
 		</div>
