0dc63d6fc11e06c0574a7bafb53392672be2bbdb
YAP CHENG WEI <yapchengwei@gmail.com>
2016-01-26 01:04:51 +0800

    product field

--- utility/leftprod.php
+++ utility/leftprod.php
@@ -1,24 +1,39 @@
 <div id="" class="new_pro_color pro_title">New Arrive
 </div>
 <?php
+	$product_sql = "select * from product";
+	$product_result = mysql_query($product_sql);
 
-	mysql_select_db($database_name, $database_connection);
-	$product_result = mysql_query("select * from user where user_email = '$user_email' and user_password = '$user_password'");
-	$search_exe = mysql_query($database_name,
-	 $product_result);
-
-		/*for($i=0;$i<10;$i++){
+	$product_check = mysql_num_rows($product_result);
+	$product_name = "";
+	if($product_check!=0){
+		$product_row = mysql_fetch_array($product_result);
+		do{
+			$product_name = $product_row['Product_Name'];
+			$product_image = $product_row['Product_Pic'];
+			$product_price = $product_row['Product_Price'];
 			echo "
 				<div id='' class='pro_div'>
 					<div id='' class='prod_pic'>
+						<img src='".$product_image."' id='' class='prod_pic_img'/>
 					</div>
 					<a href='' id='' class='prod_name'>
-					jkanaslkas
+					".$product_name."
+					</a>
+					<a href='' id='' class='prod_price'>
+					RM ".$product_price."
 					</a>
 					<a href='' class='prod_btn prod_rent'>RENT</a>
 					<a href='' class='prod_btn prod_buy'>BUY</a>
 				</div>
-			";
-		}*/
+				";
+		}while($product_row = mysql_fetch_array($product_result));
+	}
+	else {
+		echo "222";
+	}
+
+
+		/**/
 
 ?>
