a2949cf22f57bf6536bf04dd91f32889dcf4e843
Koh Jin Yi 1121115790 <jinyi_313@hotmail.com>
2016-01-30 22:35:23 +0800

    remove cart

--- test/view_cart.php
+++ test/view_cart.php
@@ -1,6 +1,8 @@
 <?php 
-
+/* ----------- ADD TO CART PAGE ----------------*/
 include("dataconn.php");
+$product_id = isset($_GET['pid']) ? $_GET['pid'] : null; 
+$action = isset($_GET['action']) ? $_GET['action'] : null; //the action
 ?>
 
 <html>
@@ -14,7 +16,9 @@ include("dataconn.php");
 <?php 
 $customer_id=$_SESSION['customer_id'];
 
-$action = isset($_GET['action']) ? $_GET['action'] : null; //the action
+
+$result=mysql_query("select * from product");
+$row=mysql_fetch_assoc($result);
 
 switch($action) 
 { 
@@ -26,11 +30,13 @@ break;
 
 case "remove":
     $_SESSION['cart'][$product_id] = 0;
-    if($_SESSION['cart'][$product_id] == 0) unset($_SESSION['cart'][$product_id]); //if the quantity is zero, remove it completely (using the 'unset' function) - otherwise is will show zero, then -1, -2 etc when the user keeps removing items. 
+    unset($_SESSION['cart'][$product_id]);
+	header("Location: payment.php");//if the quantity is zero, remove it completely (using the 'unset' function) - otherwise is will show zero, then -1, -2 etc when the user keeps removing items. 
 break;
 
 case "empty":
     unset($_SESSION['cart']); //unset the whole cart, i.e. empty the cart. 
+	header("Location: payment.php");
 break;
 }
 
@@ -39,8 +45,9 @@ break;
 if(isset($_SESSION['cart']) && $_SESSION['cart']) { 
 
 	$total = 0;
+	echo "<form name='cart' method='POST' action=''>";
     echo "<table border=\"1\" padding=\"3\" width=\"100%\" class=\"data-container\">"; 
-	 echo "<td colspan=\"4\" align=\"right\"><a href=\"$_SERVER[PHP_SELF]?action=empty\" onclick=\"return confirm('Are you sure?');\"><img src=\"image/delete.png\"/></a></td>";
+	 echo "<td colspan=\"4\" align=\"right\"><a href=\"payment.php?action=empty\" onclick=\"return confirm('Are you sure?');\"><img src=\"image/delete.png\"/></a></td>";
 
    
     foreach($_SESSION['cart'] as $product_id => $quantity) 
@@ -60,12 +67,13 @@ if(isset($_SESSION['cart']) && $_SESSION['cart']) {
             $total = $total + $cost; //add to the total cost
 
             echo "<tr>";
-            echo "<td align=\"center\"><a href=\"$_SERVER[PHP_SELF]?action=remove&pid=$product_id\"><img src=\"image/remove.png\"/></a></td>";
+           
             //show this information in table cells
             echo "<td align=\"center\">".$itemsInfo["Product_Name"]."</td>";
             //along with a 'remove' link next to the quantity - which links to this page, but with an action of remove, and the id of the current product
             echo "<td align=\"center\">$quantity </td>";
             echo "<td align=\"center\">RM $cost</td>";
+			 echo "<td align=\"center\"><a href=\"payment.php?action=remove&pid=$product_id\"><img src=\"image/remove.png\"/></a></td>";
             echo "</tr>";
         }
     }
@@ -77,10 +85,13 @@ if(isset($_SESSION['cart']) && $_SESSION['cart']) {
     echo "<td colspan=\"2\" align=\"center\">RM $total</td>";
     echo "</tr>";
     echo "</table>";
-	}?>
+	echo "</form>";
+	}
+	
+	?>
 	<br>
 	<div>
-	<span style="margin-left: 88%"><a href="addToCart.php"><input type="submit" name="shopping_more"  class="custom-button" value="Back To Shopping"/></a></span>
+	<span style="margin-left: 88%"><a href="product.php"><input type="submit" name="shopping_more"  class="custom-button" value="Back To Shopping"/></a></span>
 	</div>
 	
 
