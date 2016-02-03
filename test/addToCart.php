128171b85cdfb942a00de8e9ab0dbd6f9d1381e3
Koh Jin Yi 1121115790 <jinyi_313@hotmail.com>
2016-02-03 21:25:13 +0800

    small changes

--- test/addToCart.php
+++ test/addToCart.php
@@ -1,29 +1,27 @@
 <?php
 	include("dataconn.php");
-
 	$customer_id=$_SESSION["customer_id"]; 
 	$result=mysql_query("select * from product");
 	$row = mysql_fetch_assoc($result);
-	$total=0;
-	
 
+  $_SESSION['cart'][0]="";	
 
 	
-	if(isset($_POST["cart"])&& $_POST['pid']>0)
+	if(isset($_POST["cart"]))
 {
 	$product_id = $_POST['pid']; //the product id
 	$action = $_POST['action']; //the action
-	$quantity=$_POST['quantity'];
-	$total=$_POST['total'];
-	$total+=$quantity;
+	$_SESSION['cart'][$product_id];
+	
+
+	
+	
 	switch($action)
 	{ //decide what to do 
-
     case "add":
-        $_SESSION['cart'][$product_id] = $total;
+        $_SESSION['cart'][$product_id]++;
         $_SESSION['flash_msg'] = "Successfully added to checkout";
     break;
-
     case "remove":
         $_SESSION['cart'][$product_id] = 0;
         if($_SESSION['cart'][$product_id] == 0) unset($_SESSION['cart'][$product_id]); //if the quantity is zero, remove it completely (using the 'unset' function) - otherwise is will show zero, then -1, -2 etc when the user keeps removing items. 
@@ -31,7 +29,6 @@
 	   case "empty":
         unset($_SESSION['cart']); //unset the whole cart, i.e. empty the cart. 
     break;
-
   
 	}
 }
@@ -44,7 +41,7 @@
 
 <body>
 <a href="payment.php" target="_blank">checkout</a>
-	<form name="loginfrm" action="" method="post">
+	<form name="loginfrm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 		<?php if (isset($_SESSION['flash_msg']) && $_SESSION['flash_msg'] != NULL) 
 	{
 	    echo '<div class="alert">
@@ -55,13 +52,12 @@
 		while($row=mysql_fetch_assoc($result))
 		{
 			
-			echo "<form name='loginfrm' action='product.php' method='post'>";
+			echo "<form name='loginfrm' action='product3.php' method='post'>";
 			echo '<input type="hidden" name="pid" value="'.$row['Product_ID'].'"/>';
-
 			echo "<p>$row[Product_Name]</p><p><input type='submit' name='cart' value='Add To Cart' /></p>";
 			echo "<input type='hidden' name='quantity' value='1'/>";
 			echo "<input type='hidden' name='action' value='add'/>";
-			echo "<input type='hidden' name='total' value='".$total."'/>";
+		
 				
 			echo "</form>";
 				
@@ -72,4 +68,3 @@
 <a href="logout2.php">Logout</a>
 </body>
 </html>
-
