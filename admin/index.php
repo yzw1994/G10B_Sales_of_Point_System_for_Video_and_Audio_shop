9e713734857f078f83d8a2e5e0f2e614e4d35d11
yap zhan wei <zhanweiyap@gmail.com>
2016-02-12 03:05:30 +0800

    small changes

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,15 @@
-<!--<?php
+<?php
   include("../dataconn/dataconn.php");
   if(!isset($_SESSION))
 	{
 		session_start();
 	}
 	$user_id = $_SESSION['user_id'];
-
 	require("../dataconn/page_load.php");
-
 	$user_id = $_SESSION['user_id'];
 	$result = mysql_query("SELECT * FROM user WHERE User_ID = '$user_id'");
 	$row = mysql_fetch_assoc($result);
-?>-->
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,117 +26,131 @@ table
 }
 </style>
 </head>
+<script>
+	function adminedit()
+	{
+		document.getElementById('edit_ID').contentEditable= true;
+		document.getElementById('edit_Email').contentEditable= true;
+		document.getElementById('edit_Contact').contentEditable= true;
+		document.getElementById('edit_Name').contentEditable= true;
+    setInterval(update(str),5)
+	}
+	function update(str) {
+		if (str.length == 0) {
+        document.getElementById("edit_Email").innerHTML = "";
+        return;
+    } else {
+			var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("edit_Email").innerHTML = xmlhttp.responseText;
+            }
+        };
+        xmlhttp.open("GET", "update_profile.php?q=" + str, true);
+        xmlhttp.send();
+    }
+	}
+</script>
+<?php
+ ?>
 <body>
-  <div class="wrap">
-  	<div id="header">
-  		<div id="top">
-  			<div class="left">
-  				<!--<img src="../img/logo_bg.png" style="position:absolute; width: 490px; margin: -128px 0 0 -50px;">-->
-  				<!--<img src="../img/mmu_logo.png" alt="MMU Logo" style="position:absolute; width: 160px;">-->
-  				<!--<img src="../img/title.png" alt="System Title" style="position:absolute; width: 200px; margin-left: 200px;">-->
-  				<!--<img src="../img/logo.ico" alt="Blu Video and Audio Shop" id="" class="logo_images" style="position:absolute; width: 85px; margin: -15px 0 0 -50px;"/>-->
-          <?php echo "<img src='".$row['User_Profile_Pic']."' style='border:1px solid black' padding='5px' height='150px' width='150px'/>";?><!--call image by php-->
-          <form action="upload_profile_picture.php?user_id=<?php echo $row['User_ID'];?>" id="upload_profile" enctype="multipart/form-data" method="post">
-              <button class="file-upload">
-              <input type="file" name="edit_profile_pic" accept="image/x-png, image/gif, image/jpeg" id="edit_profile" class="file-input"/>
-              Upload Photo
-              </button>
-          </form>
-          <p id="" class="shop_name" style="font-size:25px;display:;background-color:;width:100%;color:white; margin-left:50px; margin-top:10px;">Blu Video And Audio Shop</p>
-  			</div>
-
-  		</div>
-  		<div id="nav">
-  			<ul>
-  				<li class="upp"><a href="#">Home</a></li>
-  				<li class="upp"><a href="#">Rent & Sales</a>
-  					<ul>
-  						<li><a href="">List of video</a></li>
-  						<li><a href="">Transaction History</a></li>
-  					</ul>
-  				</li>
-  				<li class="upp"><a href="#">Coming Soon</a></li>
-  			</ul>
-  		</div>
-  	</div>
-
-  	<div id="content">
-  		<div id="sidebar">
-  			<div class="box">
-  				<div class="h_title">Admin Profile</div>
-  				<div style="background:white; padding: 10px 55px;"><img src="../img/user_photo.gif" style="border:1px solid black; padding: 5px;" /></div>
-  				<p style="text-align:center; line-height: 20px;">Welcome, testing</p>
-  				<ul id="home">
-  					<li class="b1"><a class="icon profile" href="" >View profile</a></li>
-  					<li class="b1"><a class="icon logout" href="">Log Out</a></li>
-  				</ul>
-  			</div>
-  			<div class="box">
-  				<div class="h_title">Product</div>
-  				<ul>
-  					<li class="b1"><a class="icon add_product" href="">Add Product</a></li>
-  					<li class="b2"><a class="icon delete_product" href="">Delete Product</a></li>
-  					<li class="b2"><a class="icon delete_product" href="">Product List</a></li>
-  				</ul>
-  			</div>
-  			<div class="box">
-  				<div class="h_title">Graph</div>
-  				<ul>
-  					<li class="b1"><a class="icon delete_product" href="">View Report</a></li>
-  				</ul>
-  			</div>
-  			<div class="box">
-  				<div class="h_title">Manage Users</div>
-  				<ul>
-  					<li class="b1"><a class="icon add_user" href="">Add User</a></li>
-  					<li class="b2"><a class="icon delete_user" href="">Delete User</a></li>
-  				</ul>
-  			</div>
-  		</div>
-  		<div id="main">
-  			<div class="full_w">
-  				<div class="h_title">Personal Details</div>
-  				<form>
-  				<div style="float:left;">
-  					<img src="../img/user_photo.gif" style="border:1px solid black; padding: 5px; height: 150px; width: 150px;" />
-  				</div>
-  				<div>
-  					<table>
-  						<tr>
-  							<td colspan="2" style="font-size: 18pt; font-weight:bold;"><?php echo $row["User_Name"] ?>'s Profile</td>
-  						</tr>
-  						<tr>
-  							<td><b>Admin ID</b></td>
-  							<td>1121115935</td>
-  						</tr>
-  						<tr>
-  							<td><b>Email</b> </td>
-  							<td><?php echo $row["User_Email"] ?></td>
-  						</tr>
-  						<tr>
-  							<td><b>Contact No.</b></td>
-  							<td><?php echo $row["User_Phone"] ?></td>
-  						</tr>
-  						<tr>
-  							<td><b>Username</b></td>
-  							<td><?php echo $row["User_Name"] ?></td>
-  						</tr>
-              <tr>
-  							<td><b>Position</b></td>
-  							<td><?php echo $row[""] ?></td>
-  						</tr>
-  					</table>
-  					<div style="margin-left: 180px;">
-  						<button class="edit" type="button" onclick="location.href='';" style="background: #F3F3F3 url(../img/i_edit.png) no-repeat 4px center; padding-left: 25px;">Edit</button>
-  					</div>
-  				</div>
-  				</form>
-  			</div>
-  		</div>
-  	</div>
-  	<div id="footer">
-  			<p style="text-align:center;">Copyright 2016</p>
-  	</div>
-  </div>
+<div class="wrap">
+	<div id="header">
+		<div id="top">
+			<div class="left">
+				<!--<img src="../img/logo_bg.png" style="position:absolute; width: 490px; margin: -128px 0 0 -50px;">-->
+				<!--<img src="../img/mmu_logo.png" alt="MMU Logo" style="position:absolute; width: 160px;">-->
+				<!--<img src="../img/title.png" alt="System Title" style="position:absolute; width: 200px; margin-left: 200px;">-->
+				<img src="../img/logo.ico" alt="Blu Video and Audio Shop" id="" class="logo_images" style="position:absolute; width: 85px; margin: -15px 0 0 -50px;"/>
+				<p id="" class="shop_name" style="font-size:25px;display:;background-color:;width:100%;color:white; margin-left:50px; margin-top:10px;">Blu Video And Audio Shop</p>
+			</div>
+		</div>
+		<div id="nav">
+			<ul>
+				<li class="upp"><a href="../visitor/visitor.php">Home</a></li>
+				<li class="upp"><a href="#">Rent & Sales</a>
+					<ul>
+						<li><a href="">List of video</a></li>
+						<li><a href="">Transaction History</a></li>
+					</ul>
+				</li>
+				<li class="upp"><a href="#">Coming Soon</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="content">
+		<div id="sidebar">
+			<div class="box">
+				<div class="h_title">Admin Profile</div>
+				<div style="background:white; padding: 10px 55px;"><img src="../img/user_photo.gif" style="border:1px solid black; padding: 5px;" /></div>
+				<p style="text-align:center; line-height: 20px;">Welcome, testing</p>
+				<ul id="home">
+					<li class="b1"><a class="icon profile" href="" >View profile</a></li>
+					<li class="b1"><a class="icon logout" href="../visitor/visitor.php">Log Out</a></li>
+				</ul>
+			</div>
+			<div class="box">
+				<div class="h_title">Product</div>
+				<ul>
+					<li class="b1"><a class="icon add_product" href="">Add Product</a></li>
+					<li class="b2"><a class="icon delete_product" href="">Delete Product</a></li>
+					<li class="b2"><a class="icon delete_product" href="">Product List</a></li>
+				</ul>
+			</div>
+			<div class="box">
+				<div class="h_title">Graph</div>
+				<ul>
+					<li class="b1"><a class="icon delete_product" href="">View Report</a></li>
+				</ul>
+			</div>
+			<div class="box">
+				<div class="h_title">Manage Users</div>
+				<ul>
+					<li class="b1"><a class="icon add_user" href="add_user.php">User List</a></li>
+					<li class="b2"><a class="icon delete_user" href="delete_user.php">Disable User</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="main">
+			<div class="full_w">
+				<div class="h_title">Personal Details</div>
+				<form name="" method="get">
+				<div style="float:left;">
+					<img src="../img/user_photo.gif" style="border:1px solid black; padding: 5px; height: 150px; width: 150px;" />
+				</div>
+				<div>
+					<table>
+						<tr>
+							<td colspan="2" style="font-size: 18pt; font-weight:bold;">User's Profile</td>
+						</tr>
+						<tr>
+							<td><b>Admin ID</b></td>
+							<td id="edit_ID" contenteditable="false" onkeyup="keyup(this.value)"><?php echo $row['User_ID'];?></td>
+						</tr>
+						<tr>
+							<td><b>Email</b> </td>
+							<td id="edit_Email" contenteditable="false" onkeyup="keyup(this.value)"><?php echo $row['User_Email'];?></td>
+						</tr>
+						<tr>
+							<td><b>Contact No.</b></td>
+							<td id="edit_Contact" contenteditable="false" onkeyup="keyup(this.value)"><?php echo $row['User_Phone'];?></td>
+						</tr>
+						<tr>
+							<td><b>Username</b></td>
+							<td id="edit_Name" contenteditable="false" onkeyup="keyup(this.value)"><?php echo $row['User_Name'];?></td>
+						</tr>
+						</table>
+					<div style="margin-left: 180px;">
+						<button class="edit" name="editbutton" type="button" onclick="adminedit()" style="background: #F3F3F3 url(../img/i_edit.png) no-repeat 4px center; padding-left: 25px;">Edit</button>
+						</div>
+				</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+			<p style="text-align:center;">Copyright 2016</p>
+	</div>
+</div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,55 +1,73 @@
 <?php
-	include("dataconn.php");
-?>
-<!DOCTYPE html>
+
+
+
+	require("../dataconn/dataconn.php");
+
+	$reg_error_login="reg_normal";
+
+
+	if(isset($_POST['loginbtn'])){
+
+		$email=$_POST['email'];
+		$password = md5($_POST['password']);
+		$loginadm_sql = "select * from user where user_email = '".$email."' and user_password = '".$password."' and User_Privilege='2' " ;
+		$login_sql = "select * from user where user_email = '".$email."' and user_apassword = '".$password."'" ;
+
+		$login_result = mysql_query($login_sql);
+		$loginadm_result = mysql_query($loginadm_sql);
+
+		$login_check = mysql_num_rows($login_result);
+		$loginadm_check = mysql_num_rows($loginadm_result);
+
+
+		if($login_check==1){
+			$row = mysql_fetch_assoc($login_result);
+			session_start();
+			$_SESSION['user_id'] = $row["User_ID"];
+			header("Location: ../user/index.php");
+		}
+		else if ($loginadm_check==1) {
+			$row = mysql_fetch_assoc($loginadm_result);
+			session_start();
+			$_SESSION['user_id'] = $row["User_ID"];
+			header("Location: ../admin/index.php");
+
+		}
+		else {
+				$reg_error_login= "reg_error";
+		}
+} ?>
+
 <html>
 <head>
-	<title>Login Page</title>
+	<title>Log In</title>
+	<link href="../css/login_page.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/font/elegantlux/elegant_luxmager.css" type="text/css" charset="utf-8" />
+	<script src="../js/jquery-2.2.0.js"></script>
 </head>
-
 <body>
-	<form name="loginfrm" action="" method="post">
-		<p>Username : 
-			<input type="text" name="uname" size = "20" />
-		</p>
-	
-		<p>Password : 
-			<input type="password" name="upass" size = "20" />
-		</p>
-	
-		<p>
-			<input type="submit" name="loginbtn" value="Login" />
-		</p>
-	</form>
-
+	<div id="" class="login_div">
+		<div id="" class="logo_div">
+			<a href="visitor.php"><img src="../images/logo.ico" alt="Blu Video and Audio Shop" id="" class="logo_images"/></a>
+		</div>
+		<div id="" class="input_div">
+			<form name="login" method="post" action="">
+				<span>whatever slogan here...</span>
+				<input type="text" name="email" value="" id="" placeholder="Your Email..." class="input_box email" pattern="[a-z0-9._%+-]+@[a-z0-9]+.com"/>
+				<input type="password" name="password" value=""  id="" placeholder="Your Password..." class="input_box password"/>
+				<div class="forget_password">
+				<a href="forgetpassword.php" class="forget_password_text">Forget Password</a>
+				</div>
+				<div id="" class="login_error">
+					<span id="" class="<?php echo $reg_error_login;?>">Invalid Email or Password.
+					</span>
+				</div>
+				<a href="register.php"><input type="button" value="REGISTER" id="" class="btn register_btn"/></a>
+				<input type="submit" value="LOGIN" id="" name="loginbtn" class="btn login_btn"/>
+			</form>
+			<div class=""></div>
+		</div>
+	</div>
 </body>
 </html>
-
-<?php
-	if(isset($_POST['loginbtn']))
-	{
-	//`User_Name` varchar(30) NOT NULL,
-  //`User_Password` varchar(20) NOT NULL,
-	$username = $_POST["uname"];
-	$userpass = $_POST["upass"];
-	
-	$result = mysql_query("select * from user where User_Name = '$username' and User_Password = '$userpass'");
-	if(mysql_num_rows($result)!=0)
-	{
-		$row = mysql_fetch_assoc($result);
-		$_SESSION["User_ID"] = $row["User_ID"];
-		
-		header("Location: index.php");
-	}
-	else
-	{
-		?>
-		<script>
-			alert("Invalid Username or Password");
-		</script>
-		<?php
-	}
-	
-	mysql_close();
-	}
-?>
